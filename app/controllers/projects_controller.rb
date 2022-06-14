d85932b0f41c93789e17fc2c25aeab4a0f46847c
Kyllian Creppy <kyllian.creppy@outlook.fr>
2022-06-14 11:13:02 +0200

    Rails new with frontend and form gems

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,39 @@
 class ProjectsController < ApplicationController
+    def index
+       @projects = Project.all
+    end 
+
+    def show
+        @project = Project.find(params[:id])
+      end
+
+      def new
+        @project = Project.new
+      end
+
+      def create
+        @project = Project.new(params[:project])
+        @project.save
+        redirect_to project_path(@project)
+      end
+
+      def update
+        @project = Project.find(params[:id])
+        @project.update(params[:project])
+        # Will raise ActiveModel::ForbiddenAttributesError
+        redirect_to project_path(@project)
+      end
+
+      def destroy
+        @project = Project.find(params[:id])
+        @project.destroy
+        # Will raise ActiveModel::ForbiddenAttributesError
+        redirect_to projects_path
+      end
+
+      private
+
+      def project_params
+        params.require(:project).permit(:name, :description, :date)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  get 'projects', to: 'projects#projects'
-  get "skills", to: 'skills#skills'
+  get 'projects', to: 'projects#index'
+  get "projects/:id", to: "projects#show", as: :project
+  get "projects/new", to: "projects#new", as: :new_project
+  get "projects/new", to: "projects#create"
+  get  "projects/:id/edit", to: "projects#edit", as: :edit_project
+  patch "projects/:id", to: "projects#update"
+  delete "projects/new", to: "projects#destroy"
+  get "skills", to: 'skills#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
